2bbf21f57e45e741e90fe9dd5f2a055e5200f856
Justin Lee <justin@vanillacoding.co>
2021-12-27 19:09:45 +0900

    Dec 27

--- admin.js
+++ admin.js
@@ -1,17 +1,37 @@
+const PROXY_ADDRESS = "http://localhost:3000";
+
 const $postList = document.querySelector(".post-list");
 
 (async function () {
   const { data: postList } = await axios("/data/data.json");
 
+  const result = await Promise.all(
+    postList.map(async (post) => {
+      const {
+        data: { base64Source },
+      } = await axios(
+        `${PROXY_ADDRESS}/instagram/fromPostUrlToImageSource?postUrl=${post.url}`
+      );
+
+      post.imageString = base64Source;
+
+      return post;
+    })
+  );
+
+  console.log("새로운..", result);
+
   Array.from(postList).forEach((post) => {
     const postElement = document.createElement("div");
 
     postElement.classList.add("post");
+    postElement.classList.add("presenting");
 
     postElement.innerHTML = `
+      <img src="data:image/png;base64,${post.imageString}" />
       <span class="id">#${post.id}</span>
-      <label>author: </label> <input value=${post.author} />
-      <label>url: </label> <input value=${post.url} />
+      <label>author: </label> <input value=${post.author} disabled />
+      <label>url: </label> <input value=${post.url} disabled />
     `;
 
     $postList.appendChild(postElement);
---
+++ todo.md
@@ -0,0 +1,4 @@
+# Todo list
+
+- 서버로 데이터 이동시키기!
+- form에서 요청보내서 서버 데이터 CRUD 가능하도록 endpoint 생성!
