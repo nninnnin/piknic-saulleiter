f849a94e67fcf040d4ac8943761f121e903c7b4d
Justin Lee <justin@vanillacoding.co>
2022-01-06 01:59:21 +0900

    Updates on Jan 6

--- admin.js
+++ admin.js
@@ -9,9 +9,11 @@ const $postList = document.querySelector(".post-list");
     postList.map(async (post) => {
       const {
         data: { base64Source },
-      } = await axios(
-        `${PROXY_ADDRESS}/instagram/postUrlToImageSource?postUrl=${post.url}`
-      );
+      } = await axios(`${PROXY_ADDRESS}/instagram/`, {
+        params: {
+          postUrl: post.url,
+        },
+      });
 
       post.imageString = base64Source;
 
@@ -28,10 +30,16 @@ const $postList = document.querySelector(".post-list");
     postElement.classList.add("presenting");
 
     postElement.innerHTML = `
-      <img src="data:image/png;base64,${post.imageString}" />
-      <span class="id">#${post.id}</span>
-      <label>author: </label> <input value=${post.author} disabled />
-      <label>url: </label> <input value=${post.url} disabled />
+      <img
+        src="data:image/png;base64,${post.imageString}"
+      />
+
+      <span class="id">
+        #${post.id}
+      </span>
+
+      <input value=${post.author} disabled />
+      <input value=${post.url} disabled />
     `;
 
     $postList.appendChild(postElement);
